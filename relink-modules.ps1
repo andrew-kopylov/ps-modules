1508629aac206a55ed845ee55cd20c4dfa3d37f0
Andrey Kopylov <kopylov.andr@gmail.com>
2019-10-08 11:05:32 +0300

    Delete junction modules

--- relink-modules.ps1
+++
@@ -1,9 +0,0 @@
-$Exceptions = '.git', 'config', 'modules', 'logs'
-$Folders = Get-ChildItem -Path $PSScriptRoot -Directory | Where-Object -FilterScript {$_.BaseName -notin $Exceptions}
-foreach ($Item in $Folders) {
-    $ModulesPath = $Item.FullName + '\modules'
-    if (Test-Path -Path $ModulesPath) {
-        Remove-Item -Path $ModulesPath -Recurse -Force
-    }
-    New-Item -Path $ModulesPath -ItemType Junction -Value ($PSScriptRoot + '\modules')
-}
