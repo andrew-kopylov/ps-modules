891b2ea687820694cd582ba459ca51738e16ac3f
andrew <kopylov.andr@gmail.com>
2019-11-04 17:20:33 +0300

    pg-scripts: bagfix. Delete wal shifted on backup point more then one positions.

--- scripts-pg/archive-delete.ps1
+++ scripts-pg/archive-delete.ps1
@@ -7,7 +7,7 @@ $Log = Get-PgLog -Dir $LogDir -Name 'DeleteBackup'
 
 Out-PgLog -Log $Log -Mark 'Start' -Text 'Search last actual backup'
 
-# Get config settings
+# GET DELETE CONFIG SETTINGS
 $ScriptConfigFile = Add-PgPath -Path $ScriptItem.DirectoryName -AddPath config, ($ScriptItem.BaseName + '.json')
 $ScriptConfigDelete = Get-Content -Path $ScriptConfigFile | ConvertFrom-Json
 
@@ -28,81 +28,110 @@ Out-PgLog -Log $Log -Mark 'Info' -Text ('hold d' + $HoldDays + ' h' + $HoldHours
 
 $HoldTime = New-TimeSpan -Days $HoldDays -Hours $HoldHours -Minutes $HoldMinutes
 $ThresholdDate = (Get-Date) - ($HoldTime)
-$ThresholdName = 'walbackup-' + $ThresholdDate.ToString('yyyyMMdd-HHmmss')
+$WalThresholdName = 'walbackup-' + $ThresholdDate.ToString('yyyyMMdd-HHmmss')
 
 Out-PgLog -Log $Log -Mark 'Info' -Text ('threshold date: ' + $ThresholdDate.ToString('dd.MM.yyyy HH:mm'))
 
-$WalItems = Get-ChildItem -Path $ScriptConfigWal.archive -Filter 'walbackup-????????-??????.*.backup.*' -Recurse `
-| Where-Object -FilterScript {$_.Name -gt $ThresholdName} | Sort-Object -Property Name -Descending | Select-Object -Last 1
+# GET LAST BACKUP POINT (NOT TO DELETE)
+$LastBackupPoint = Get-ChildItem -Path $ScriptConfigWal.archive -Filter 'walbackup-????????-??????.*.backup.*' -File -Recurse `
+| Where-Object -FilterScript {$_.Name -gt $WalThresholdName} | Sort-Object -Property Name -Descending | Select-Object -Last 1
 
-if ($WalItems.count -eq 0) {
-    Out-PgLog -Log $Log -Mark 'End' -Text 'No backup for delete'
+if ($LastBackupPoint.count -eq 0) {
+    Out-PgLog -Log $Log -Mark 'End' -Text 'No last backup point to keep'
     return
 }
 
-Out-PgLog -Log $Log -Mark 'LastBackup' -Text $WalItems.FullName
+Out-PgLog -Log $Log -Mark 'Info' -Text ('Last backup point to keep: ' + $LastBackupPoint.FullName)
 
-$WalBackupLabelItem = $WalItems[0]
-$WalPattern = '(?<prefix>walbackup-\d{8}-\d{6})\.(?<wal>[0-9a-fA-F]{24})\.(?<label>[0-9a-fA-F]{8})\.backup\.(?<ext>\w+)'
+$LastBackupPointItem = $LastBackupPoint[0]
+$BackupPointPattern = '(?<prefix>walbackup-\d{8}-\d{6})\.(?<wal>[0-9a-fA-F]{24})\.(?<point>[0-9a-fA-F]{8})\.backup\.(?<ext>\w+)'
 
-if ($WalBackupLabelItem.Name -match $WalPattern) {
-    $WalPrefix = $Matches.prefix
+if ($LastBackupPointItem.Name -match $BackupPointPattern) {
+    $BackupPointPrefix = $Matches.prefix
     $WalName = $Matches.wal
-    $BackupLabel = $Matches.label
+    $BackupPoint = $Matches.label
     $BackupExt = $Matches.ext
 }
 else {
-    Out-PgLog -Log $Log -Mark 'info' -Text 'Bad backup name'
+    Out-PgLog -Log $Log -Mark 'Error' -Text 'Bad backup name'
     return
 }
 
+# GET LAST WAL POINT (NOT TO DELETE): delete all prefer wal.
+$LastWal = Get-ChildItem -Path $ScriptConfigWal.archive -Filter ('walbackup-????????-??????.' + $WalName + '.*') -File -Recurse `
+| Where-Object -FilterScript {$_.Name -lt $BackupPointPrefix} | Sort-Object -Property Name -Descending | Select-Object -First 1
+
+$WalPattern = '(?<prefix>walbackup-\d{8}-\d{6})\.(?<wal>[0-9a-fA-F]{24})\.(?<ext>\w+)'
+
+$WalPrefix = ''
+if ($LastWal.count -gt 0) {
+    $LastWalItem = $LastWal[0]
+    Out-PgLog -Log $Log -Mark 'Info' -Text ('Last wal to keep ' + $LastWalItem.FullName)
+    if ($LastWalItem.FullName -match $WalPattern) {
+        $WalPrefix = $Matches.prefix
+    }
+    else {
+        Out-PgLog -Log $Log -Mark 'Error' -Text 'Bad backup name'
+        return       
+    }
+}
+else {
+    Out-PgLog -Log $Log -Mark 'Info' -Text 'No wal prefer last backup'
+}
+
+if ([String]::IsNullOrEmpty($WalPrefix)) {
+    $WalPrefix = $BackupPointPrefix
+}
+
+# DELETE BACKUP
 Out-PgLog -Log $Log -Mark 'DeleteBak' -Text 'Start'
 
-$BackupLabelItems = Get-ChildItem -Path $ScriptConfigWal.archive -Filter 'walbackup-????????-??????.*.backup.*' -Recurse `
-| Where-Object -FilterScript {($_.Name -lt $WalPrefix)} | Sort-Object -Property Name -Descending | Select-Object -First 1
-if ($BackupLabelItems.count -gt 0) {
+$BakPrefix = $BackupPointPrefix -replace 'walbackup-', 'basebackup-'
 
-    $BackupLabelItem = $BackupLabelItems[0]
+$BackupItems = Get-ChildItem -Path $ScriptConfigBase.archive -Filter 'basebackup-????????-??????.*' -File -Recurse `
+| Where-Object -FilterScript {($_.Name -lt $BakPrefix)} | Sort-Object -Property Name
 
-    Out-PgLog -Log $Log -Mark 'DeleteBak' -Text ('Last backup label to delete: ' + $BackupLabelItem.FullName)
 
-    if ($BackupLabelItem.Name -match $WalPattern) {
-        $BakPrefix = $Matches.prefix
-    }
+if ($BackupItems.count -gt 1) {
 
-    $BackupItems = Get-ChildItem -Path $ScriptConfigBase.archive -Filter 'basebackup-????????-??????.*' -Recurse `
-    | Where-Object -FilterScript {($_.Name -lt $BakPrefix)} | Sort-Object -Property Name
+    # Skip last backup - it is current last backup.
+    $LastBackupToKeep = $BackupItems | Sort-Object -Property Name | Select-Object -Last 1
+
+    Out-PgLog -Log $Log -Mark 'Info' -Text ('Last backup to keep: ' + $LastBackupToKeep.FullName)
 
     foreach ($BackupItem in $BackupItems) {
+        if ($BackupItem -eq $LastBackupToKeep) {
+            continue
+        }
         Out-PgLog -Log $Log -Mark 'DeleteBak' -Text $BackupItem.FullName
         Remove-Item -Path $BackupItem.FullName -Force
         if (Test-Path -Path $BackupItem.FullName) {
-            Out-PgLog -Log $Log -Mark 'Error' -Text 'Not deleted backup file'
+            Out-PgLog -Log $Log -Mark 'Error' -Text 'Hasn''t deleted backup file'
         }
     }
 
 }
 else {
-    Out-PgLog -Log $Log -Mark 'info' -Text 'No backup labels'
+    Out-PgLog -Log $Log -Mark 'Info' -Text 'No backups to delete'
 }
 
-
+# DELETE WAL
 Out-PgLog -Log $Log -Mark 'DeleteWal' -Text 'Start'
 
-$WalItems = Get-ChildItem -Path $ScriptConfigWal.archive -Filter 'walbackup-????????-??????.*' -Recurse `
-| Where-Object -FilterScript {($_.Name -lt $WalPrefix) -and ($_.Name -notlike ('*' + $WalName + '*'))} | Sort-Object -Property Name
+$WalItems = Get-ChildItem -Path $ScriptConfigWal.archive -Filter 'walbackup-????????-??????.*' -File -Recurse `
+| Where-Object -FilterScript {($_.Name -lt $WalPrefix)} | Sort-Object -Property Name
 
 if ($WalItems.count -gt 0) {
     foreach ($WalItem in $WalItems) {
         Out-PgLog -Log $Log -Mark 'DeleteWal' -Text $WalItem.FullName
         Remove-Item -Path $WalItem.FullName -Force
         if (Test-Path -Path $WalItem.FullName) {
-            Out-PgLog -Log $Log -Mark 'Error' -Text 'Not deleted wal file'
+            Out-PgLog -Log $Log -Mark 'Error' -Text 'Hasn''t deleted wal file'
         }
     }
 }
 else {
-    Out-PgLog -Log $Log -Mark 'info' -Text 'No backup wal'
+    Out-PgLog -Log $Log -Mark 'Info' -Text 'No backup wal to delete'
 }
 
 Out-PgLog -Log $Log -Mark 'End' -Text 'OK'
