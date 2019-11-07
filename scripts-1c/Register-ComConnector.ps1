fe89e395d9f16a9e523ef99a9312f3cf24b11cb8
Andrey Kopylov <kopylov.andr@gmail.com>
2019-11-08 01:05:19 +0300

    Регистрация компоненты с повышением прав

---
+++ scripts-1c/Register-ComConnector.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module 1c-module -Force
+
+$WinUsr = [System.Security.Principal.WindowsPrincipal]([System.Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $Winusr.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process -FilePath 'powershell' -ArgumentList ('-f "' + $PSCommandPath + '"') -Verb RunAs
+    break
+}
+
+# Current Powershell command file
+$PSCmdFile = Get-Item -Path $PSCommandPath
+
+#Config
+$ConfigDir = $PSCmdFile.DirectoryName + '\config'
+$ConfigData = Get-Content -Path ($ConfigDir + '\config.json') | ConvertFrom-Json 
+
+Register-1CComConnector -V8 $ConfigData.V8
+
+$ComConnector = Get-1CComConnector -V8 $ConfigData.V8
+if ($ComConnector -ne $null) {
+    ('OK - ' + $ConfigData.V8) | Out-Host 
+}
+else {
+    'ERROR' | Out-Host
+}
+
+Start-Sleep -Seconds 1
--- scripts-1c/regsiter-comconnector.ps1
+++
@@ -1,5 +0,0 @@
-﻿$ModulePath = $PSScriptRoot + "\modules\1c-module.ps1"
-Import-Module $ModulePath -Force
-
-Register-1CComConnector
-
