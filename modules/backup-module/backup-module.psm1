e4312bc2d626c65ccf5e97c203b9733b09fc2263
Andrey Kopylov <kopylov.andr@gmail.com>
2019-12-11 23:55:18 +0300

    backup-module: Переработан модуль: основные процедуру Get-BakPolicy - определяет политику бэкапирования - формат имени бэкапов, каталог, сроки хранения бэкапов - дней, недель, месяцев, лет. Remove-BakFiles - удаляет устаревшие файлы бэкапов - единственный параметр $BakPolicy.

--- modules/backup-module/backup-module.psm1
+++ modules/backup-module/backup-module.psm1
@@ -1,109 +1,138 @@
-﻿
-function Remove-BakFiles($BackupDir, $BackupExt) {
+﻿function Get-BakPolicy() {
+    param (
+        $Path,
+        $DatePattern,
+        $Prefix,
+        $Postfix,
+        $Daily,
+        $Weekly,
+        $Monthly,
+        $Annual
+    )
+    @{
+        Path = $Path;
+        DatePattern = $DatePattern;
+        Prefix = $Prefix;
+        Postfix = $Postfix;
+        Daily = $Daily;
+        Weekly = $Weekly;
+        Monthly = $Monthly;
+        Annual = $Annual;
+    }
+}
 
-    $RemovedBackups = @();
+function Remove-BakFiles($BakPolicy) {
+    $RemovedFiles = @()
+    $FilesWithDate = Get-BakFilesWithDate -BakPath $BakPolicy.Path -DatePattern $BakPolicy.DatePattern -Prefix $BakPolicy.Prefix -Postfix $BakPolicy.Postfix
+    $FilesToRemove = Get-BakFilesToRemove -BakFilesWithDate $FilesWithDate -BakPolicy $BakPolicy
+    foreach ($Item in $FilesToRemove) {
+        $RemovedFiles += $Item.FullName
+        Remove-Item -Path $Item.Fullname -Force
+    }
+    $RemovedFiles
+}
 
-    $WcNum6 = Repeat-BakString -Str '[0-9]' -Count 6
-    $WcNum8 = Repeat-BakString -Str '[0-9]' -Count 8
-    $WcBackup = '*_' + $WcNum8 + '_' + $WcNum6 + '_[dwmy][0-9]*.' + $BackupExt
+function Get-BakFilesWithDate($BakPath, $DatePattern, $Prefix, $Postfix) {
 
-    $ItemList = Get-ChildItem -Path ($BackupDir + '\*') -Include $WcBackup -File
-    foreach ($Item in $ItemList) {
-    
-        $Backup = Parce-BakDate -BackupName ($Item.Name)
-        if ($Backup.OK -ne 1) {continue}
+    # yyyyMMdd-HHddss -> (?<y>\d{4})(?<mon>\d{2})(?<d>\d{2})-(?<h>\d{2})(?<min>\d{2})(?<s>\d{2})
+    $DatePattern = $DatePattern.Replace('yyyy', '(?<y>\d{4})')
+    $DatePattern = $DatePattern.Replace('yy', '(?<y>\d{2})')
+    $DatePattern = $DatePattern.Replace('MM', '(?<mon>\d{2})')
+    $DatePattern = $DatePattern.Replace('dd', '(?<d>\d{2})')
+    $DatePattern = $DatePattern.Replace('HH', '(?<h>\d{2})')
+    $DatePattern = $DatePattern.Replace('mm', '(?<min>\d{2})')
+    $DatePattern = $DatePattern.Replace('ss', '(?<s>\d{2})')
+    $DatePattern = '(\D|^)' + $DatePattern + '(\D|$)'
 
-        $TimeSpan = New-TimeSpan -Start $Backup.BackupDate -End (Get-Date)
-        if ($TimeSpan.TotalDays -gt $Backup.StoreDays) {
-            $RemovedBackups += $Item.FullName 
-            Remove-Item -Path $Item.FullName
+    $BakFiles = Get-ChildItem -Path $BakPath -File -Filter ($Prefix + '*' + $Postfix)
+
+    $FilesWithDate = @()
+
+    foreach ($BakItem in $BakFiles) {
+
+        $BaseName = $BakItem.BaseName
+        if ($BaseName -match $DatePattern) {
+            $Year = $Matches.y
+            if (($Year -gt 0) -and ($Year -lt 100)) {
+                $Year = 2000 + $Year
+            }  
+            $BakDate = [datetime]::new($Year, $Matches.mon, $Matches.d, $Matches.h, $Matches.min, $Matches.s)
+            $FileWithDate = @{
+                Item = $BakItem;
+                Date = $BakDate;
+            }
+            $FilesWithDate += New-Object PSCustomObject -Property $FileWithDate
         }
     }
 
+    $FilesWithDate
 }
 
-function Parce-BakDate($BackupName, $BackupExt) {
+function Get-BakFilesToRemove($BakFilesWithDate, $BakPolicy) {
 
-    $Result = @{OK = 0; Period = ""; StorePeriods = 0; StoreDays = 0; BackupDate = $null; BakName = ""}
+    $FilesToKeep = @()
+    $FilesToKeep += Get-BakFilesToKeep -BakFilesWithDate $BakFilesWithDate -Period 'y' -PeriodCount $BakPolicy.Annual
+    $FilesToKeep += Get-BakFilesToKeep -BakFilesWithDate $BakFilesWithDate -Period 'm' -PeriodCount $BakPolicy.Monthly
+    $FilesToKeep += Get-BakFilesToKeep -BakFilesWithDate $BakFilesWithDate -Period 'w' -PeriodCount $BakPolicy.Weekly
+    $FilesToKeep += Get-BakFilesToKeep -BakFilesWithDate $BakFilesWithDate -Period 'd' -PeriodCount $BakPolicy.Daily
 
-    $MatchBacupName = '(.+)_(\d{8})_(\d{6})_([dwmy])(\d+)\.' + $BackupExt + '$'
-
-    if ($BackupName -match $MatchBacupName) {
-        
-        $Result.OK = 1
-        $Result.BakName = $Matches.1  
-        $Result.Period = $Matches.4
-        $Result.StorePeriods = [int]$Matches.5
-        $Result.StoreDays = $Result.StorePeriods * (Get-BakDaysInPeriod -Period $Result.Period)
-         
-        # Date
-        $Date = $Matches.2
-        $Year = [int]$Date.Substring(0, 4)
-        $Month = [int]$Date.Substring(4, 2)
-        $Day = [int]$Date.Substring(6, 2)
-       
-        # Time
-        $Time = $Matches.3
-        $Hour = [int]$Time.Substring(0, 2)
-        $Minute = [int]$Time.Substring(2, 2)
-        $Second = [int]$Time.Substring(4, 2)
-
-        $Result.BackupDate = (Get-Date -Year $Year -Month $Month -Day $Day -Hour $Hour -Minute $Minute -Second $Second)
-
-    }
-
-    $Result
-}
-
-function Find-BakFile($BakName, $BackupDir, $BackupExt, $Period, [int]$StorePeriods, $BackupDate = $null) {
-
-    $WcNum6 = Repeat-BakString -Str '[0-9]' -Count 6
-
-    if ($BackupDate -eq $null) {$BackupDate = Get-Date}
-   
-    $ArrWcDay = @();
-    
-    # Define wildcards of the days.
-    if ($Period -eq 'd') {
-        $ArrWcDay += $BackupDate.ToString('yyyyMMdd')
-    }
-    elseif ($Period -eq 'w') {
-        $StartOfWeek = $BackupDate.AddDays(-$BackupDate.DayOfWeek + 1)
-        for ($i = 1; $i -le 7; $i++) {
-            $ArrWcDay += ($StartOfWeek.AddDays($i - 1)).ToString('yyyyMMdd')
+    $BakFilesToRemove = @()
+    foreach ($BakFileWithDate in $BakFilesWithDate) {
+        if ($BakFileWithDate.Item -notin $FilesToKeep) {
+            $BakFilesToRemove += $BakFileWithDate.Item
         }
     }
-    elseif ($Period -eq 'm') {
-        $ArrWcDay += $BackupDate.ToString('yyyyMM') + '[0-9][0-9]'
-    }
-    elseif ($Period -eq 'y') {
-        $ArrWcDay += $BackupDate.ToString('yyyy') + '[0-9][0-9][0-9][0-9]'
-    }
-    else {}
-  
-    $Result = @();  
-    foreach ($WcDay in $ArrWcDay) {
-        $WcBackup = $BakName + '_' + $WcDay + '_' + $WcNum6 + '_' + $Period + $StorePeriods.ToString() + '.' + $BackupExt
-        $Result = Get-ChildItem -Path ($BackupDir + '\*') -Include $WcBackup -File
-        if ($Result.Count -gt 0) {break}
-    }
 
-    $Result
+    $BakFilesToRemove
 }
 
-function Repeat-BakString($Str, $Count) {
-    $ResStr = ''
-    for ($i = 1; $i -le $Count; $i++) {
-        $ResStr = $ResStr + $Str;
+function Get-BakFilesToKeep($BakFilesWithDate, $Period, $PeriodCount) {
+    
+    $NextPeriodBegin = Get-BakPeriodBegin -Date (Get-Date) -Period $Period
+    $NextPeriodEnd = Get-BakNextPeriod -Date $NextPeriodBegin -Period $Period
+
+    $BakFilesToKeep = @()
+    for ($i = 0; $i -le $PeriodCount; $i++) {
+        $PeriodBegin = $NextPeriodBegin 
+        $PeriodEnd = $NextPeriodEnd
+        $FindedItems = $BakFilesWithDate.Where({($_.Date -ge $PeriodBegin) -and ($_.Date -lt $PeriodEnd)}) | Sort-Object -Property Date | Select-Object -Property Item -First 1
+        if ($FindedItems.Item -ne $null) {
+            $BakFilesToKeep += $FindedItems.Item
+        }
+        # Next period is erler
+        $NextPeriodBegin = Get-BakNextPeriod -Date $PeriodBegin -Period $Period -PeriodCount -1 
+        $NextPeriodEnd = $PeriodBegin
     }
-    $ResStr
+    $BakFilesToKeep
 }
 
-function Get-BakDaysInPeriod($Period) {
-    if     ($Period -eq 'd') {$Result = 1}
-    elseif ($Period -eq 'w') {$Result = 7}
-    elseif ($Period -eq 'm') {$Result = 31}
-    elseif ($Period -eq 'y') {$Result = 366}
-    else {$Result = 0}
-    $Result
+function Get-BakPeriodBegin([datetime]$Date, $Period) {
+    if ($Period -like 'd') {
+        return [datetime]::new($Date.Year, $Date.Month, $Date.Day) 
+    }
+    elseif ($Period -like 'w') {
+        $Date = Get-BakPeriodBegin -Date $Date -Period 'd'
+        return $Date.AddDays(-$Date.DayOfWeek.value__ + 1)
+    }
+    elseif ($Period -like 'm') {
+        return [datetime]::new($Date.Year, $Date.Month, 1)
+    }
+    elseif ($Period -like 'y') {
+        return [datetime]::new($Date.Year, 1, 1)
+    }
+}
+
+function Get-BakNextPeriod([datetime]$Date, $Period, [int]$PeriodCount = 1) {
+    if ($Period -like 'd') {
+        return $Date.AddDays($PeriodCount)
+    }
+    elseif ($Period -like 'w') {
+        return $Date.AddDays(($PeriodCount * 7))
+    }
+    elseif ($Period -like 'm') {
+        return $Date.AddMonths($PeriodCount)
+    }
+    elseif ($Period -like 'y') {
+        return $Date.AddYears($PeriodCount)
+    }
 }
