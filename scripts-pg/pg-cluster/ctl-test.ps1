2be1727800b2c1c5c4b71990a2a7a3e9c38beac8
Andrey Kopylov <kopylov.andr@gmail.com>
2020-02-10 01:56:44 +0300

    pg-clusterctl: тестовые сценарии

--- scripts-pg/pg-cluster/ctl-test.ps1
+++ scripts-pg/pg-cluster/ctl-test.ps1
@@ -5,6 +5,8 @@
 # Backup all bases form all clusters
 #D:\git\ps-modules\scripts-pg\pg-cluster\ctl.ps1 backup-bases
 
+D:\git\ps-modules\scripts-pg\pg-cluster\ctl.ps1 Add-Cluster -c test12 -p 5442 -pwd 'test5441' -initcluster
+
 # Backup only base from one cluster.
 #D:\git\ps-modules\scripts-pg\pg-cluster\ctl.ps1 backup-bases -c test3 -b test5433-base0
 
