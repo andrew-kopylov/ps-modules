02353330a0cc6372ddade28f7ec790221c79c646
Andrey Kopylov <kopylov.andr@gmail.com>
2020-03-28 01:10:50 +0300

    1c-module: Расширены возможности скрипта публикации webinst
    Новые возможности:
    - Добавлен запрос параметров аутентификации
    - Добавлен запрос web-пути публикации
    - Добавлена возможность указания шаблона vrd-файла

--- scripts-1c/webinst.ps1
+++ scripts-1c/webinst.ps1
@@ -9,15 +9,32 @@ if (-not $Winusr.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Admini
 $ConfigFile = Add-1CPath -Path $PSScriptRoot -AddPath config, config.json
 $Config = Get-Content -Path $ConfigFile | ConvertFrom-Json 
 
-$Ref = Read-Host -Prompt 'Введите имя базы для публикации (ver ' + $Config.v8 + ', server ' + $Config.srvr ')'
+$Ref = Read-Host -Prompt ('Введите имя базы для публикации (ver ' + $Config.v8 + ', server ' + $Config.srvr + ')')
 
-$WebPath = $Ref.Replace('_', '-')
-$Conn = Get-1CConn -V8 $Config.v8 -Srvr $Config.srvr -Ref $Ref
+$Usr = Read-Host -Prompt ('Имя пользователя базы  (base "' + $Ref + '")')
+if (-not [string]::IsNullOrEmpty($Usr)) {
+    $Pwd = Read-Host -Prompt ('Пароль пользователя (usr "' + $Usr + '")')
+}
+else {
+    $Usr = $null
+    $Pwd = $null
+}
+
+$WebPath = Read-Host -Prompt ('Введите web-путь к базе для публикации (оставить пустым для формирования пути по умолчанию)')
+
+if ([string]::IsNullOrEmpty($WebPath)) {
+    $WebPath = $Ref.Replace('_', '-')
+    ('Web-путь к базе: ' + $WebPath) | Out-Host
+}
+$Conn = Get-1CConn -V8 $Config.v8 -Srvr $Config.srvr -Ref $Ref -Usr $Usr -Pwd $Pwd
 
 $CmdFile = Add-1CPath -Path $PSScriptRoot -AddPath webinst.cmd
 $VrdFile = Add-1CPath -Path $PSScriptRoot -AddPath default.vrd
 if (-not (Test-Path -Path $VrdFile)) {
     $VrdFile = $null
+}
+else {
+    ('Шаблон файла публикации: ' + $VrdFile) | Out-Host
 }
 
 $Result = Invoke-1CWebInst -Conn $Conn -Command publish -Ws iis -WsPath $WebPath -Descriptor $VrdFile
