f3746688e91b4b897acab5153e291f72cf2c6f13
Andrey Kopylov <kopylov.andr@gmail.com>
2020-03-28 01:11:29 +0300

    1c-module: Добавлен скрипт удаления сеансовых данных.

---
+++ scripts-1c/remove-seancedata.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module 1c-module -Force
+
+# Старт скрпита с административными правами.
+$WinUsr = [System.Security.Principal.WindowsPrincipal]([System.Security.Principal.WindowsIdentity]::GetCurrent())
+if (-not $Winusr.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    Start-Process -FilePath 'powershell' -ArgumentList ('-f "' + $PSCommandPath + '"') -Verb RunAs
+    break
+}
+
+# Остановка служб 1С
+Stop-1CService
+
+Start-Sleep -Seconds 1
+
+# Удаление оставшихся процессов сервера 1С.
+Get-Process | Where-Object -FilterScript {$_.Name -like 'rphost*'} | % {$_.Kill()}
+Get-Process | Where-Object -FilterScript {$_.Name -like 'rmngr*'} | % {$_.Kill()}
+Get-Process | Where-Object -FilterScript {$_.Name -like 'ragent*'} | % {$_.Kill()}
+
+Start-Sleep -Seconds 1
+
+# Удаление сеансовых данных.
+Remove-Item -Path "C:\Program Files\1cv8\srvinfo\reg_*\snccntx*\*" -Force
+
+# Запуск службы 1С
+Start-1CService
