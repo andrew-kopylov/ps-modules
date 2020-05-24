9505d26eb1409035ac45b7b74e40489bc49462d0
Andrey Kopylov <kopylov.andr@gmail.com>
2020-05-24 03:04:49 +0300

    Добавлен скрипт удаления старых бэкапов по времени хранения и дате последнего изменения.

---
+++ scripts-bak/Remove-OldBackups.ps1
@@ -0,0 +1,32 @@
+﻿
+$BackupDir = "D:\to_archive"
+$KeepDaysDevBases = 1
+$KeepDaysTechBases = 10
+
+$TechBases = @()
+$TechBases += 'erp-crm-model'
+$TechBases += 'erp-crm-test'
+$TechBases += 'erp-crm-preprod'
+
+$BakDirs = Get-ChildItem -Path $backupdir -Directory
+
+foreach ($Dir in $BakDirs) {
+
+    if ($Dir.BaseName -in $TechBases) {
+        $KeepDays = $KeepDaysTechBases
+    }
+    else {
+        $KeepDays = $KeepDaysDevBases
+    }
+    $DeleteDate = (Get-Date).AddDays(-$KeepDays-1)
+
+    $AllBakFiles = Get-ChildItem -Path $Dir.FullName -Filter *.bak
+    $FilesToDelete = $AllBakFiles | Where-Object -FilterScript {$_.LastWriteTime -lt $DeleteDate}
+
+    $AllBakFilesCount = ([object[]]$AllBakFiles).Count
+    $FilesToDeleteCount = ([object[]]$FilesToDelete).Count
+    if ($AllBakFilesCount -gt $FilesToDeleteCount) {
+        $FilesToDelete | % {Remove-Item -Path $_.FullName}
+    }
+}
+
